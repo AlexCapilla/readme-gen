5733636fc3b514aba7de65f11d4ddab8f0791d7a
AlexCapilla <acapilla93@gmail.com>
2024-04-29 21:36:59 -0700

    created function to write README file

--- index.js
+++ index.js
@@ -32,14 +32,38 @@ const questions = [
         message: "What kind of license should your project have?",
         choices: ["MIT", "APACHE", "GPL", "None"]
     },
+    {
+        type: "input",
+        name: "installation",
+        message: "What command should be run to install dependencies?"
+    },
+    {
+        type: "input",
+        name: "test",
+        message: "What command should be run to test?"
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "What does the user need to know about using the repo?"
+    },
+    {
+        type: "input",
+        name: "contributing",
+        message: "What does the user need to know about contributing to the repo?"
+    },
 
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    return fs.writeFileSync(path.join(process.cwd(), fileName), data)
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    
+}
 
 // Function call to initialize app
 init();
