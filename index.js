e192e05014d58a34b38194b7ffd4da70fd69490b
AlexCapilla <acapilla93@gmail.com>
2024-04-29 21:29:35 -0700

    installed packages and added questions needed for readme file

--- index.js
+++ index.js
@@ -1,7 +1,39 @@
 // TODO: Include packages needed for this application
+const path = require("path");
+const fs = require("fs");
+const inquirer = require("inquirer");
+const genMarkDown = require("genMarkDown");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        name: "github",
+        message: "What is your Github username?"
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is your email?"
+    },
+    {
+        type: "input",
+        name: "title",
+        message: "What is your project's name?"
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Please write a short description of your project."
+    },
+    {
+        type: "list",
+        name: "license",
+        message: "What kind of license should your project have?",
+        choices: ["MIT", "APACHE", "GPL", "None"]
+    },
+
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
